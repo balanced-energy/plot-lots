a380fad7db0e31750dff187ca30fb190c256bc59
Camden Christopherson <CamdenLv@gmail.com>
2021-04-10 23:14:35 -0700

    Linked project website to html index page. Started Data Preparation

--- plotlots/views.py
+++ plotlots/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'plotlots/index.html', context)
