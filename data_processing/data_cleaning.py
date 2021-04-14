599c3d812a0041178b3def7065ccf4d818ebe8d6
Camden Christopherson <CamdenLv@gmail.com>
2021-04-13 22:48:03 -0700

    Fixed Sizes to include sf

--- data_processing/data_cleaning.py
+++ data_processing/data_cleaning.py
@@ -14,30 +14,51 @@ df = df.dropna(subset=['point'])
 
 # REGEX
 # Match pattern to capture acreage pattern
-# (.)[0-9] followed by optional spaces then "AC", "Acres", "AC"
+# [0-9]?,?[0-9]?(.)[0-9]
+# followed by optional spaces and "AC", "Acres", "AC"
 def split_acres(s):
-    pattern = r'(\d*\.?\d+\s?)(?=AC|Ac|ac)'
-    # Return matching acreage values in acrs_list
-    acres_list = re.findall(pattern, s)
-    # Find max acreage and check if acres_list exists else 0
-    acr_max = max(acres_list, key=lambda x: float(x))\
-        if acres_list else 0
-    return acr_max
+    pattern = r'(\d*,?\d*\.?\d+\s?)(?=AC|Ac|ac)'
+    acres = re.findall(pattern, s)
+    if acres:
+        size_max = max(acres, key=lambda x: float(x.replace(',', '')))
+        return size_max + ' Acres'
+    else:
+        return 0
 
 
-# Split off Acreage from 'Legal Description' series
+# Match pattern to capture acreage pattern
+# [0-9]?,?[0-9]?(.)[0-9]
+# followed by optional spaces and "SF", "Sf", "sf"
+def split_sf(s):
+    pattern = r'(\d*,?\d*\.?\d+\s?)(?=SF|Sf|sf)'
+    sq_ft = re.findall(pattern, s)
+    if sq_ft:
+        size_max = max(sq_ft, key=lambda x: float(x.replace(',', '')))
+        return size_max + ' Sf'
+    else:
+        return 0
+
+
+# Split Size of acres or square ft off from String input
+def split_size(s):
+    if split_acres(s):
+        return split_acres(s)
+    else:
+        return split_sf(s)
+
+
+# Split off Size from 'Legal Description' series
 df['Size'] = df['Legal Description'].apply(
-    lambda x: split_acres(x)
+    lambda x: split_size(x)
 )
 
-
-# Creat Hover_info for map, Size and minimum bid
+# Create Hover_info (Size, Minimum Bid)
 df['Hover_info'] = df['Size'].astype(str) \
-                   + ' -- AC, BID: ' \
+                   + ' --- MinBid: ' \
                    + df['Minimum Bid'].astype(str)
 
 
 # Save file
-df.to_csv(r'data/final_data.csv', index = False, header=True)
+df.to_csv(r'data/final_data.csv', index=False, header=True)
 
 print("Created -- final_data.csv")
