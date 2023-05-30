6458f607ede4184011376e946849bd65cd20ec56
Camden Christopherson <CamdenLv@gmail.com>
2023-05-30 16:31:12 -0600

    adjusting zoom levels

--- run/folium_map.py
+++ run/folium_map.py
@@ -7,14 +7,14 @@ csv_file = 'data/final_data.csv'
 df = pd.read_csv(csv_file)
 
 # Initialize folium map
-#m = folium.Map(location=df[["latitude", "longitude"]].mean().to_list(), zoom_start=2)
-m = folium.Map(location=df[["latitude", "longitude"]])
+m = folium.Map(location=df[["latitude", "longitude"]].mean().to_list(), zoom_start=15)
+#m = folium.Map(location=df[["latitude", "longitude"]])
 
-# Test default zoom
-sw = df[["latitude", "longitude"]].min().values.tolist()
-ne = df[["latitude", "longitude"]].max().values.tolist()
-
-m.fit_bounds([sw, ne])
+# # Test default zoom
+# sw = df[["latitude", "longitude"]].min().values.tolist()
+# ne = df[["latitude", "longitude"]].max().values.tolist()
+#
+# m.fit_bounds([sw, ne])
 # Cluster close points, create a cluster overlay with MarkerCluster, add to m
 marker_cluster = MarkerCluster().add_to(m)
 
